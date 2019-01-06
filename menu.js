cf2e4579d879195f73b3afbdf7855d3774307dc4
Vlasov997 <bovlas1455@gmail.com>
2019-01-06 19:24:04 +0200

    all end

---
+++ menu.js
@@ -0,0 +1,16 @@
+
+
+(function($){
+
+    $( document ).ready(function() {
+
+        $("#gumburger_menu").click(function (e) {
+
+            $(".sub-menu").slideToggle();
+        });
+    });
+
+
+
+
+})(jQuery);
